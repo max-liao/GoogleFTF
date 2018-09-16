9ddf0ab6db29b598c6a367cbbc4558e7e315eb59
Max Liao <max.liao@gmail.com>
2018-09-16 10:49:28 -0400

    Bootstrap Init

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,3 +1,21 @@
 var test = new Vue({
     el:"#TestComponent",
-})
+    data: {
+        isActive: true,
+        hasError: false,
+        activeColor: 'blue',
+        fontSize: '30px',
+        computed: {
+            classObject: function () {
+                return {
+                    active: this.isActive && !this.hasError,
+                    'text-danger': ((this.isActive && this.hasError) ? true : false)
+                }
+            }
+        },
+        message: 'Hello World'
+    },
+    errorClass: function () {
+        return { 'text-danger': ((this.isActive && this.hasError) ? true : false) }
+    }
+});
